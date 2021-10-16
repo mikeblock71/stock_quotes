a1ec0ebdcfbeefbb0484de86d2732a7154d3c2e9
Mike <mikeblock71@yahoo.com>
2021-10-16 18:22:38 -0400

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+root 'pages#home'
+
 Rails.application.routes.draw do
   get 'stock_quotes/index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
